55e58729c80ccd982c7e8b4d913cd77672fc9365
whoiscolek <cogk98@gmail.com>
2026-05-05 01:26:12 -0500

    Update config.js

--- api/config.js
+++ api/config.js
@@ -3,7 +3,7 @@
 export const ODDS_API_KEY = process.env.ODDS_API_KEY || "d894360865dfb9af7b8cf1712b353b05";
 
 export const ODDS_BASE_URL = "https://api.the-odds-api.com/v4";
-export const NBA_SCOREBOARD_BASE_URL = "https://cdn.nba.com/static/json/liveData/scoreboard";
+export const NBA_SCOREBOARD_URL = "https://cdn.nba.com/static/json/liveData/scoreboard/todaysScoreboard_00.json";
 export const NBA_INJURY_REPORT_URL = "https://official.nba.com/nba-injury-report-2025-26-season/";
 
 export const ROUND_VALUES = {
